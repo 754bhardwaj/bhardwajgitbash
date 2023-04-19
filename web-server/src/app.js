884519f87ccf5d7b63dfe97d2f8ea3a7132e4a5a
754bhardwaj <harshbhardwaj754@gmail.com>
2023-04-19 12:25:52 +0530

    Commit_One

---
+++ web-server/src/app.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../template/views');
+const partialsPath = path.join(__dirname,'../template/partials');
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(publicDirectoryPath))
+app.get('', (req, res) => {
+res.render('index', {
+title: 'ExpressApp',
+name: 'MyName'
+})
+})
+app.get('/about', (req, res) => {
+res.render('about', {
+title: 'About Me',
+name: 'MyName'
+})
+})
+
+app.get('/help', (req, res) => {
+res.render('help', {
+helpText: 'This is some helpful text.',
+title: 'Help',
+name: 'MyName'
+})
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+    title: '404',
+    name: 'MyName',
+    errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+    title: '404',
+    name: 'MyName',
+    errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+console.log('Server is up on port 3000.')
+})
